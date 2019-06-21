8a06f57b54ff256e3ce37568a4f0f8e30b9f555f
raptorsqueak <r4pt0squ34k@gmail.com>
2019-06-21 16:51:50 -0700

    Adding some ptf functionality

---
+++ Setup/ptf/modules/wireless/airgeddon.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+#####################################
+# Installation module for ghost-phisher
+#####################################
+
+# AUTHOR OF MODULE NAME
+AUTHOR="Raptorsqueak"
+
+# DESCRIPTION OF THE MODULE
+DESCRIPTION="Install airgeddon"
+
+# INSTALL TYPE GIT, SVN, FILE DOWNLOAD
+# OPTIONS = GIT, SVN, FILE
+INSTALL_TYPE="GIT"
+
+# LOCATION OF THE FILE OR GIT/SVN REPOSITORY
+REPOSITORY_LOCATION="https://github.com/v1s1t0r1sh3r3/airgeddon.git"
+
+# WHERE DO YOU WANT TO INSTALL IT
+INSTALL_LOCATION="airgeddon"
+
+# DEPENDS FOR DEBIAN INSTALLS
+DEBIAN="git,iw,aircrack-ng,xterm,hostapd-wpe,reaver,dsniff,ettercap,pixiewps,expect,asleap"
+
+# DEPENDS FOR FEDORA INSTALLS
+FEDORA=""
+
+# COMMANDS TO RUN AFTER
+AFTER_COMMANDS=""
